e7558eaf11021fcb2f1fe1382a2b192aafdf2834
Rishik_17 <rushikeshpatil.rp7@gmail.com>
2025-03-25 11:18:24 +0530

    Add files via upload

---
+++ Library_MnG/Insertion.sql
@@ -0,0 +1,116 @@
+-- Library System Management SQL Project
+
+-- CREATE DATABASE library;
+
+-- Create table "Branch"
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+            branch_id VARCHAR(10) PRIMARY KEY,
+            manager_id VARCHAR(10),
+            branch_address VARCHAR(30),
+            contact_no VARCHAR(15)
+);
+
+
+-- Create table "Employee"
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees
+(
+            emp_id VARCHAR(10) PRIMARY KEY,
+            emp_name VARCHAR(30),
+            position VARCHAR(30),
+            salary DECIMAL(10,2),
+            branch_id VARCHAR(10),
+            FOREIGN KEY (branch_id) REFERENCES  branch(branch_id)
+);
+
+
+-- Create table "Members"
+DROP TABLE IF EXISTS members;
+CREATE TABLE members
+(
+            member_id VARCHAR(10) PRIMARY KEY,
+            member_name VARCHAR(30),
+            member_address VARCHAR(30),
+            reg_date DATE
+);
+
+
+
+-- Create table "Books"
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+(
+            isbn VARCHAR(50) PRIMARY KEY,
+            book_title VARCHAR(80),
+            category VARCHAR(30),
+            rental_price DECIMAL(10,2),
+            status VARCHAR(10),
+            author VARCHAR(30),
+            publisher VARCHAR(30)
+);
+
+
+
+-- Create table "IssueStatus"
+DROP TABLE IF EXISTS issued_status;
+CREATE TABLE issued_status
+(
+            issued_id VARCHAR(10) PRIMARY KEY,
+            issued_member_id VARCHAR(30),
+            issued_book_name VARCHAR(80),
+            issued_date DATE,
+            issued_book_isbn VARCHAR(50),
+            issued_emp_id VARCHAR(10),
+            FOREIGN KEY (issued_member_id) REFERENCES members(member_id),
+            FOREIGN KEY (issued_emp_id) REFERENCES employees(emp_id),
+            FOREIGN KEY (issued_book_isbn) REFERENCES books(isbn) 
+);
+
+
+
+-- Create table "ReturnStatus"
+DROP TABLE IF EXISTS return_status;
+CREATE TABLE return_status
+(
+            return_id VARCHAR(10) PRIMARY KEY,
+            issued_id VARCHAR(30),
+            return_book_name VARCHAR(80),
+            return_date DATE,
+            return_book_isbn VARCHAR(50),
+            FOREIGN KEY (return_book_isbn) REFERENCES books(isbn)
+);
+
+
+--rows insertion can be done manually or directly importing the data
+
+--for eg. inserting for below two columns
+
+INSERT INTO branch(branch_id, manager_id, branch_address, contact_no) 
+VALUES
+('B001', 'E109', '123 Main St', '+919099988676'),
+('B002', 'E109', '456 Elm St', '+919099988677'),
+('B003', 'E109', '789 Oak St', '+919099988678'),
+('B004', 'E110', '567 Pine St', '+919099988679'),
+('B005', 'E110', '890 Maple St', '+919099988680');
+
+
+INSERT INTO employees(emp_id, emp_name, position, salary, branch_id) 
+VALUES
+('E101', 'John Doe', 'Clerk', 60000.00, 'B001'),
+('E102', 'Jane Smith', 'Clerk', 45000.00, 'B002'),
+('E103', 'Mike Johnson', 'Librarian', 55000.00, 'B001'),
+('E104', 'Emily Davis', 'Assistant', 40000.00, 'B001'),
+('E105', 'Sarah Brown', 'Assistant', 42000.00, 'B001'),
+('E106', 'Michelle Ramirez', 'Assistant', 43000.00, 'B001'),
+('E107', 'Michael Thompson', 'Clerk', 62000.00, 'B005'),
+('E108', 'Jessica Taylor', 'Clerk', 46000.00, 'B004'),
+('E109', 'Daniel Anderson', 'Manager', 57000.00, 'B003'),
+('E110', 'Laura Martinez', 'Manager', 41000.00, 'B005'),
+('E111', 'Christopher Lee', 'Assistant', 65000.00, 'B005');
+
+select * from books
+
+
+
